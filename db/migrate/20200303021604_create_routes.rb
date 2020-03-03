a285a953ce182916cabe4902d6b7efa1118666e9
Will Kaspar <Will@Aang.local>
2020-03-02 20:16:52 -0600

    generates Route and Address model migrations

---
+++ db/migrate/20200303021604_create_routes.rb
@@ -0,0 +1,11 @@
+class CreateRoutes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :routes do |t|
+      t.uuid :provider
+      t.float :geo_coordinates
+      t.json :response
+
+      t.timestamps
+    end
+  end
+end
