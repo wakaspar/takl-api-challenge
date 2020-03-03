8a3dd16925d3dfcac16d772869975dd577c44608
Will Kaspar <Will@Aang.local>
2020-03-02 20:57:40 -0600

    removes unncessary serialization from app/models/route.rb, sets up routes for Address and Route models, and adds Geocoder gem to the Gemfile

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,12 +1,8 @@
 class Route < ApplicationRecord
 
-  # serialize geo coordinates into an Array
-  serialize :geo_coordinates, Array
-
   # association to addresses
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses
-
 
   #validations
   #TODO
