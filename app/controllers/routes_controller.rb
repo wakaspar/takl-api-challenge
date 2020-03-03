b9090045fc7be811f8cafcf82f51a51463e380ba
Will Kaspar <Will@Aang.local>
2020-03-03 15:41:02 -0600

    RoutesController cleanup, commenting, and fleshing out get_optimal_route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -9,17 +9,15 @@ class RoutesController < ApplicationController
 
   # GET /routes/:id
   def show
-    p "GET by :id = " + params[:id]
     render json: @route
   end
 
   # POST /routes
   def create
-
+    # get delivery Route from HERE API
     get_optimal_route
-
+    # create new Route in db
     @route = Route.new(route_params)
-
     if @route.save
       render json: @route, status: :created, location: @route
     else
@@ -42,33 +40,64 @@ class RoutesController < ApplicationController
     @route.destroy
   end
 
+
   private
 
+
+  # sets Route on show, update, and destroy
   def set_route
     @route = Route.find(params[:id])
   end
 
+  # GET optimal delivery route
   def get_optimal_route
-    p "INSIDE GET_OPTIMAL_ROUTE"
-    # get coordinates for a Route's addresses
+    # base URI for request to HERE API
+    @base_uri = 'https://wse.ls.hereapi.com/2/findsequence.json?'
+    # returns @delivery_route_coordinates, an array of coordinates from addresses POSTed
     get_geo_coords
+    # converts array of coordinates into strings, renamed as destinations
+    @destinations = @delivery_route_coordinates.map { |coords|
+      coords.reject(&:blank?).join(',')
+    }
+    # prepares starting coordinates for HERE API request
+    @start_point = params[:route][:geo_coordinates]
+    @start_point = @start_point.map { |coord|
+      coord.to_s
+    }
+    @start_point = 'start=' + @start_point[0] + ',' + @start_point[1] + '&'
 
-    p @geo_array
+    # prepares destinations for HERE API request
+    @destinations = @destinations.map.with_index(1) do |dest, idx|
+      dest = 'destination' + idx.to_s + '=' + dest + '&'
+    end
+
+    # SANITY CHECK OUTPUT BEFORE HTTPARTY REQUEST
+    p '<== STARTING POINT ==>'
+    p @start_point
+    p '<== DELIVERY ROUTE ==>'
+    p @destinations
+
+
+    # @res = HTTParty.get('')
+    # puts @res.body
+
   end
 
+  # Uses Geocoder to get coordinates for a deliery route's addresses
   def get_geo_coords
-    # addresses from request params
-    @address_array = params[:route][:addresses_attributes]
-    # uses Geocoder to map addresses to coordinates
-    @geo_array = @address_array.map { |address|
-      full_address = address.values.join(', ')
-      geo_coords = Geocoder.search(full_address)
-      geo_coords = geo_coords.first.coordinates
+    # grabs addresses from request params
+    @delivery_route_addresses = params[:route][:addresses_attributes]
+    # uses Geocoder to map delivery addresses to coordinates
+    @delivery_route_coordinates = @delivery_route_addresses.map { |address|
+      address = address.values.join(', ')
+      address = Geocoder.search(address)
+      address = address.first.coordinates
      }
-     # return array of coordinates
-    return @geo_array
+    # returns array of delivery coordinates
+    return @delivery_route_coordinates
   end
 
+  # requires and permits Route parameters
   def route_params
     params.require(:route).permit( :provider,
                                    :response,
